d3d3fcc0f1f5f4fc3976f65b01fe9ca36fce8d7e
Christopher Moore <chris@nuwavecommerce.com>
2018-03-21 12:29:38 -0700

    registered provider

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Nuwave\Lighthouse\Providers\LighthouseServiceProvider::class,
 
         /*
          * Application Service Providers...
